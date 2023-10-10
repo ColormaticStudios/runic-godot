3e2afc1de654ff72094d860f0636140f20f9c399
Zakarya <zakaryacarroll.contact@gmail.com>
2023-10-09 18:33:33 -0700

    Add a leveling system

--- data/character/character.gd
+++ data/character/character.gd
@@ -6,16 +6,21 @@ var accel = 10
 var immobile = false
 
 var inventory = {
+	"points": 0,
 	"stone": 0,
 	"wood": 0,
 	"food": 0
 }
 var status = {
 	"health": 100,
-	"stamina": 100
+	"stamina": 100,
+	"level": 1
 }
 var current_tool = "none"
 
+
+func get_total_points_for_next_level(): #Yes, this name is very long. Deal with it
+	return status.level*10
 
 func change_tool(tool):
 	current_tool = tool
@@ -40,15 +45,25 @@ func _process(delta):
 				var hit_object = $body_pivot/raycast.get_collider()
 				if hit_object.type == "resource":
 					inventory[hit_object.resource_type] += 1
+					inventory.points += 1
 					hit_object.get_node("animation").play("hit")
 	
 	#update inventory HUD
 	$hud/inventory/stone_label.text = str(inventory.stone)
 	$hud/inventory/wood_label.text = str(inventory.wood)
+	$hud/level/points.text = str(inventory.points) + "/" + str(get_total_points_for_next_level())
+	$hud/level/level_bar.value = inventory.points
 	
 	#update status HUD
 	$hud/bars/health.value = status.health
 	$hud/bars/stamina.value = status.stamina
+	$hud/level/level.text = "Level " + str(status.level)
+	$hud/level/level_bar.max_value = get_total_points_for_next_level()
+	
+	#level up
+	if inventory.points >= (get_total_points_for_next_level()):
+		inventory.points -= get_total_points_for_next_level()
+		status.level += 1
 
 func _input(event):
 	if event is InputEventMouseMotion:
